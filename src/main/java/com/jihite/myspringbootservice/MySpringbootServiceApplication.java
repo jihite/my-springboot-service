dfe0950d11d1488e0881ff1ea7032c3725819951
jihite <wanghao2012ying@126.com>
2019-05-27 00:10:01 +0800

    src

---
+++ src/main/java/com/jihite/myspringbootservice/MySpringbootServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jihite.myspringbootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jihite/myspringbootservice/MySpringbootServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jihite.myspringbootservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySpringbootServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
